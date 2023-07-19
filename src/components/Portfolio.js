e31e1a913c5c8baa610598b7fd46413c1965a489
jdel-18 <joshuadelves1@yahoo.com>
2023-07-18 22:33:09 -0500

    update

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -8,6 +8,10 @@ import Project from './pages/Project';
 import Contact from './pages/Contact';
 import Image1 from '../components/images/avengers.png';
 import Image2 from '../components/images/booktrkr.png';
+import Image3 from '../components/images/Readme.png';
+import Image4 from '../components/images/JateScreenshot.png';
+import Image5 from '../components/images/Ecommerce.png';
+import Image6 from '../components/images/SQLapi.png';
 import '../App.css';
 
 const Portfolio = () => {
@@ -19,7 +23,7 @@ const Portfolio = () => {
     }
     if (currentPage === 'Project') {
       return (
-        <div>
+        <div className="project-container">
           {projects.map((project, index) => (
             <Project
               key={index}
@@ -40,6 +44,7 @@ const Portfolio = () => {
     }
   };
   
+  
 
   const handlePageChange = (page) => setCurrentPage(page);
 
@@ -48,13 +53,31 @@ const Portfolio = () => {
       title: 'Marvels FIGs, Assemble!',
       image: Image1,
       deployLink: 'https://jdel-18.github.io/first-group-project/',
-      repoLink: 'https://github.com/jdel-18/first-group-project'
     },
     {
       title: 'BookTrackr',
       image: Image2,
       deployLink: 'https://bookmanagement.herokuapp.com/',
-      repoLink: 'https://github.com/nhunguyen-debug/Bookmanagement-system-Group3'
+    },
+    {
+      title: 'README Generator',
+      image: Image3,
+      repoLink: 'https://github.com/jdel-18/Auto-README-generator'
+    },
+    {
+      title: 'E Commerce Back End',
+      image: Image5,
+      repoLink: 'https://github.com/jdel-18/Challenge-E-commerce-mapping'
+    },
+    {
+      title: 'SQL network api',
+      image: Image6,
+      repoLink: 'https://github.com/jdel-18/SQL-network-api'
+    },
+    {
+      title: 'J.A.T.E. (Just Another Text Editor)',
+      image: Image4,
+      deployLink: 'https://whispering-cove-04575-c4c9149efb4f.herokuapp.com/'
     },
   ];
 
--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -1,19 +1,22 @@
 import React from 'react';
 
 const Project = ({ title, image, deployLink, repoLink }) => {
+  const openRepoLink = () => {
+    window.open(repoLink, '_blank');
+  };
+
   return (
-    <div className="project">
+    <div className="project" onClick={openRepoLink}>
       <img src={image} alt={title} />
       <h3>{title}</h3>
       <a href={deployLink} target="_blank" rel="noopener noreferrer">
         Deployed App
       </a>
-      <a href={repoLink} target="_blank" rel="noopener noreferrer">
-      <img src= '../components/images/github.png' alt="GitHub Repository" />
+      <a href={repoLink} target="_blank" rel="noopener noreferrer" className="repo-link">
+        GitHub Repository
       </a>
     </div>
   );
 };
-
 
 export default Project;
